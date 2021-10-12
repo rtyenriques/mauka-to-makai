3deb00e06a137ee61d26e9010f2521aef506b9a6
= <rtyenriques@gmail.com>
2021-10-12 02:50:27 -0700

    seeded database with beaches from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'rest-client'
+require 'open-uri'
+
+url = URI.open('https://hawaiibeachsafety.com/rest/conditions.json')
+beach_ids = open(url).read
+beaches = JSON.parse(beach_ids)
+
+beaches.each do |b|
+
+    # beach_id = beaches[b]
+    # url_beach = 'https://hawaiibeachsafety.com/rest/conditions.json'
+    # beach_info = open(url_beach).read
+    # beach = JSON.parse(beach_info)
+    # new_beach = Beach.create(
+    #     :name => beach['beach']
+    # )
+    new_beach = Beach.create(
+         :name => b['beach'],
+         :island => b['island'],
+         :shore => b['shore'],
+         :temp => b['temp'],
+         :weather => b['weather'],
+         :surf => b['surf']
+    )
+
+    
+ 
+end
+   
+ 
+
+
+
+
+
+
+
+
